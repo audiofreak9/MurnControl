b1b7a6c1a42e9bddec7a2380a8401ee3429cd417
audiofreak9 <audiofreak9@gmail.com>
2019-04-05 21:14:50 -0400

    Update pi-temperature-sensor.groovy

--- smartapps/audiofreak9/pi-temperature-sensor.src/pi-temperature-sensor.groovy
+++ smartapps/audiofreak9/pi-temperature-sensor.src/pi-temperature-sensor.groovy
@@ -18,7 +18,7 @@ definition(
     namespace: "audiofreak9",
     author: "Corey Murnaghan",
     description: "REST endpoint for Pi to update a Simulated Temperature Sensor",
-    category: "Convenience",
+    category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/murnaghan/Skill60x60.png",
     iconX2Url: "https://s3.amazonaws.com/murnaghan/Skill120x120.png",
     iconX3Url: "https://s3.amazonaws.com/murnaghan/Skill120x120.png",
